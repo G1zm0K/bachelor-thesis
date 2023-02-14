8a295994cdcc1b42014b8fec22091448ab162af9
Gijs Kopmeiners <g1zm0k@pop-os.localdomain>
2023-02-14 14:34:19 +0100

    No iframe support is working + more prints

--- crawler.py
+++ crawler.py
@@ -5,24 +5,30 @@ from find_banner import find_banner
 
 def crawl(driver, selectors, sites, accept_words_list):
     for index, site in enumerate(sites):
+        print("Getting site: " + str(site))
         driver.get(site)
+        print("Sleeping for 5 seconds")
         sleep(5)
         banner_data = {"potential_elements": []}
-        elements = driver.find_elements(By.CSS_SELECTOR, selectors)
 
         #Find the element that contains accept word
-        find_banner(driver,elements,accept_words_list,banner_data)
+        print("Finding element...")
+        find_banner(driver,selectors,accept_words_list,banner_data)
 
         #If not found, switch to iframes and try again
         if not "element_found" in banner_data:
-            iframe_contents = driver.find_elements(By.CSS_SELECTOR, "iframe")
-            for content in iframe_contents:
-                print("Switching to iframe: {}".format(content.id))
-                driver.switch_to.frame(content)
-                find_banner(driver,elements,accept_words_list,banner_data)
+            iframes = driver.find_elements(By.CSS_SELECTOR, "iframe")
+            print("No initial element found, now looking in these iframes: " + str(iframes))
+            for iframe in imframes:
+                print("Switching to iframe: {}".format(iframe.id))
+                driver.switch_to.frame(iframe)
+                print("Finding element...")
+                find_banner(driver,selectors,accept_words_list,banner_data)
                 driver.switch_to.default_content()
 
-        if "element_found" in banner_data:
-            print(banner_data["element_found"])
+        if not "element_found" in banner_data:
+            print("No element found")
 
+        sleep(1)
+        print("Making screenshot")
         driver.save_screenshot('screenshots/' + str(index) + '.png')
