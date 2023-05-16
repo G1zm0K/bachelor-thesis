456ed4ad0c4b9d946c8c90542b0dd0699d7b06da
Gijs Kopmeiners <g1zm0k@pop-os.localdomain>
2023-05-16 15:23:09 +0200

    data now gets added to data.csv + other bug fixes

--- find_banner.py
+++ find_banner.py
@@ -1,18 +1,15 @@
 from selenium.webdriver.common.by import By
 import logging
 
-def find_banner(driver,selectors,accept_words_list,banner_data):
+def find_banner(driver,selectors,words_list):
     logging.info('Selecting elements to search')
     elements = driver.find_elements(By.CSS_SELECTOR, selectors)
+    
     logging.info('Searching selected elements')
     for e in elements:
-        if e.text.lower().strip(" ✓›!\n>") in accept_words_list:
-            banner_data["potential_elements"].append({"tag_name": e.tag_name,
-                                                        "text": e.text,
-                                                        "size": e.size,
-                                                        })
-            banner_data["element_found"] = e.tag_name
+        if e.text.lower().strip(" ✓›!\n>") in words_list:
             driver.execute_script("arguments[0].style.border='4px solid red'", e)
             logging.info('Element found: ' + str(e.tag_name) + ' with text: ' + str(e.text.lower().strip(" ✓›!\n>")))
-            break
+            return(True,str(e.tag_name),str(e.text.lower().strip(" ✓›!\n>")))
+    return(False,'','')
             
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from make_list import make_wordlist
 from crawler import crawl
 import logging
+import pandas as pd
 
-GLOBAL_SELECTOR = "a, button, div, span, form, p"
+GLOBAL_SELECTOR = "button, a, p, div, span, form"
 ACCEPT_WORDS_LIST = "wordlists/consent_nl.txt"
 REJECT_WORDS_LIST = "wordlists/reject_nl.txt"
-WEBSITE_LIST = "websites/nl_minus_nl_original.txt"
+WEBSITE_LIST = "websites/test.txt"
 
 logging.basicConfig(filename='info_log.log', encoding='utf-8', level=logging.INFO)
 
@@ -23,9 +24,13 @@ driver = webdriver.Firefox()
 logging.info('Driver started')
 
 logging.info('Starting crawl...')
-crawl(driver, GLOBAL_SELECTOR, sites, accept_words_list, reject_words_list)
+data = crawl(driver, GLOBAL_SELECTOR, sites, accept_words_list, reject_words_list, data)
 logging.info('Finished crawl')
 
 logging.info('Shutting down driver...')
 driver.quit()
 logging.info('Driver shut down')
+
+logging.info('Sending data to file...')
+data.to_csv('data.csv', index=False)
+logging.info('Data sent to file')
